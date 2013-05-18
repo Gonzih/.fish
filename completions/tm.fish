48bcfe71917fff66cbe25304bd10c1f929e29d04
Max Gonzih <gonzih@gmail.com>
2013-05-18 15:17:59 +0300

    Move completions to completions/ dir

---
+++ completions/tm.fish
@@ -0,0 +1,2 @@
+make_completion tm 'tmux attach -t' # complete tm as tmux attach
+complete -c tm -x # don't complete tm with files
--- config.fish
+++
@@ -1,3 +0,0 @@
-make_completion g git
-make_completion tm 'tmux attach -t' # complete tm as tmux attach
-complete -c tm -x # don't complete tm with files
