4eaa7b6642f0b4c71296e03f0582d784e25d96b7
Max Gonzih <gonzih@gmail.com>
2013-11-04 21:07:15 +0300

    Keys

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -1,8 +1,10 @@
 bind \ct history-search-backward
 bind \ch history-search-forward
+
 bind \cd backward-word
 bind \cn forward-word
-bind \cp yank
+
+bind \cy yank
 
 bind \ed backward-kill-line
 bind \en kill-line
