2850932416981a044c2561d5a32b7f7fd57ef8b9
Max Gonzih <gonzih@gmail.com>
2014-01-06 22:11:26 +0300

    Update tf2 function

--- functions/team-fortress.fish
+++ functions/team-fortress.fish
@@ -1,4 +1,4 @@
 function team-fortress
 	sh ~/Team\ Fortress\ 2/tf/pvhud.sh
-	steam steam://rungameid/440 -useforcedmparms -noforcemaccel -noforcemspd
+	steam steam://rungameid/440 -useforcedmparms -noforcemaccel -noforcemspd -novid
 end
