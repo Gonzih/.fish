e6157f1afc3c973416a8b8e4e96656a35a01c1d0
Max Gonzih <gonzih@gmail.com>
2014-03-08 17:08:36 +0300

    Set tyalpha to "on" in rand-tybg

--- functions/rand-tybg.fish
+++ functions/rand-tybg.fish
@@ -1,6 +1,7 @@
 function rand-tybg
 	set -l wall (rand-wallpaper)
 	if test -x /usr/bin/tybg
+		fish -c "sleep 1; and /usr/bin/tyalpha on" &
 		/usr/bin/tybg $wall
 		echo "Setting wallpaper to \"$wall\""
 	end
