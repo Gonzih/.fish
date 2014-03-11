3108e71756c59cd056b956a7121e406d8f8e1869
Max Gonzih <gonzih@gmail.com>
2014-03-11 18:20:16 +0100

    Revert "Add more file and dirs checks to rand wallpaper"
    This reverts commit ded881cab61d70e7aa77c6ccd212a49bbeb7a2e6.

--- functions/rand-wallpaper.fish
+++ functions/rand-wallpaper.fish
@@ -1,8 +1,5 @@
 function rand-wallpaper
 	set -l wallpapers ~/Dropbox/Public/pics/wall
-
-	if test -d $wallpapers
-		set -l wallpaper (find $wallpapers/ -type f | sort -R | head -n 1)
-		echo $wallpaper
-	end
+	set -l wallpaper (find $wallpapers/ -type f | sort -R | head -n 1)
+	echo $wallpaper
 end
