95a730866b63ee5ab0f7bf38084342f24ba41773
Max Gonzih <gonzih@gmail.com>
2014-05-02 09:47:34 +0200

    Init rbenv instead of rvm in config.fish

--- config.fish
+++ config.fish
@@ -2,7 +2,7 @@ if test $TERM != "screen-256color"
 	set -gx TERM xterm-256color
 end
 
-rvm use system 2>&1 > /dev/null
+status --is-interactive; and . (rbenv init -|psub)
 
 set fish_key_bindings fish_user_key_bindings
 set fish_bind_mode insert
