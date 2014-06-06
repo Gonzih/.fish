ff006c28baa7115e4aa6c71ef4bd527bd8604fc2
Max Gonzih <gonzih@gmail.com>
2014-06-06 16:14:56 +0200

    Don't stash on dotfiles update

--- functions/dotfiles-update.fish
+++ functions/dotfiles-update.fish
@@ -1,6 +1,6 @@
 function dotfiles-update
 	for dir in ~ ~/.vim ~/.mc ~/.config/fish ~/.xmonad ~/.mutt
-		cd $dir; and git stash; and git pull; and git stash apply
+		cd $dir; and git pull
 	end
 
 	vundle-update
