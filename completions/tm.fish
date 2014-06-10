bb5eec44306820bbffd1d2ae3d8733e75efa8241
Max Gonzih <gonzih@gmail.com>
2014-06-10 20:16:41 +0200

    Fix tm command completion

--- completions/tm.fish
+++ completions/tm.fish
@@ -1,2 +1,6 @@
-make_completion tm 'tmux attach -t' # complete tm as tmux attach
+function __fish_tmux_sessions --description 'available sessions'
+	tmux list-sessions -F "#S	#{session_windows} windows created: #{session_created_string} [#{session_width}x#{session_height}]#{session_attached}" | sed 's/0$//;s/1$/ (attached)/' ^/dev/null
+end
+
 complete -c tm -x # don't complete tm with files
+complete -c tm -a '(__fish_tmux_sessions)' -d 'target-session'
