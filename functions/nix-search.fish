2f33678ae34e4228c8bc4aef01f9a8d0e1426f61
Max Gonzih <gonzih@gmail.com>
2015-01-02 18:07:36 +0100

    Cache nix-search data

--- functions/nix-search.fish
+++ functions/nix-search.fish
@@ -1,3 +1,14 @@
 function nix-search
-	nix-env -qaP --description '*' | grep -i "$argv"
+	set -l user (whoami)
+	set -l file "/tmp/nix-search-cache-$user"
+
+	# Delete file if older than 3 days
+	find $file -type f -mtime +3 -delete 2>1 >/dev/null
+
+	if test -e $file
+		grep -i "$argv" $file | sed 's/  \+/|/g' | column -t -s'|'
+	else
+		echo "Recreating nix search cache."
+		nix-env -qaP --description '*' > $file
+	end
 end
--- functions/nix-upgrade.fish
+++ functions/nix-upgrade.fish
@@ -1,3 +1,8 @@
 function nix-upgrade
 	sudo nixos-rebuild switch --upgrade
+	nix-env --upgrade
+
+	set -l user (whoami)
+	set -l file "/tmp/nix-search-cache-$user"
+	rm -f $file
 end
