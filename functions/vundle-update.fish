ef0cf6958f24a67b23b0c4fb2631e26b24b8129e
Max Gonzih <gonzih@gmail.com>
2015-01-16 16:36:01 +0100

    -f

--- functions/vundle-update.fish
+++ functions/vundle-update.fish
@@ -1,5 +1,5 @@
 function vundle-update
 	vim -c "execute \"PluginUpdate\" | q | q"
-	cd ~/.vim/bundle/vimproc.vim; and rm autoload/*.so; and make
+	cd ~/.vim/bundle/vimproc.vim; and rm -f autoload/*.so; and make
 	cd
 end
