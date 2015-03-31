13d8274059bbd7ab1c993182712d38482d0ecef7
Max Gonzih <gonzih@gmail.com>
2015-03-31 15:49:55 +0200

    Better flux script

--- functions/flux-restart.fish
+++ functions/flux-restart.fish
@@ -1,5 +1,12 @@
 function flux-restart
-	kill (ps aux | grep fluxgui.py | awk '{print $2}')
-	rm -f /tmp/fluxgui_$USER.pid
+	set -l IDS (ps aux | grep fluxgui.py | awk '{print $2}')
+	echo "Killing gui processes"
+	kill $IDS
+	echo "Killing flux processes"
+	pkill xflux
+	echo "Removing pid files"
+	rm -f /tmp/fluxgui_gnzh.pid
+	echo "Stating new flux instance"
 	nohup fluxgui &
+	sleep 5
 end
