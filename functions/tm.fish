b750278d31c2cc605dba3662b35cbcb34474d694
Max Gonzih <gonzih@gmail.com>
2015-09-25 18:12:11 +0200

    Add tm function

--- functions/tm.fish
+++ functions/tm.fish
@@ -1,3 +1,11 @@
 function tm
-    tmux -2 -u has -t $argv[1]; and tmux -2 -u attach -t $argv[1]; or tmux -2 -u new -s $argv[1]
+  if test $argv[1] = "."
+    set __TM_SESSION_NAME (pwd | sed "s#^$HOME/##")
+  else
+	  set __TM_SESSION_NAME $argv[1]
+  end
+
+  set -l name $__TM_SESSION_NAME
+
+  tmux -2 -u has -t $name; and tmux -2 -u attach -t $name; or tmux -2 -u new -s $name
 end
