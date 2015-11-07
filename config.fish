9afe8d58ab7e3317b2c55b607c55df0784bc59d3
Max Gonzih <gonzih@gmail.com>
2015-11-07 17:18:18 +0100

    Make rbenv init call optional

--- config.fish
+++ config.fish
@@ -2,7 +2,9 @@ if test $TERM != "screen-256color"
 	set -gx TERM xterm-256color
 end
 
-status --is-interactive; and . (rbenv init -|psub)
+if test (which rbenv 2>/dev/null)
+  status --is-interactive; and . (rbenv init -|psub)
+end
 
 set -g fish_key_bindings fish_user_key_bindings
 set -g fish_bind_mode insert
