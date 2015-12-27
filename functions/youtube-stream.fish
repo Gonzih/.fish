fdc209f0b5ca4eb0135122b712a6bfb94fa67241
Max Gonzih <gonzih@gmail.com>
2015-12-27 12:58:23 +0100

    Fix youtube stream function

--- functions/youtube-stream.fish
+++ functions/youtube-stream.fish
@@ -1,3 +1,3 @@
 function youtube-stream
-	mplayer -cookies -cookies-file /tmp/ytcookie.txt (youtube-dl -g --cookies=/tmp/ytcookie.txt $argv[1])
+	wget (youtube-dl -g --cookies=/tmp/ytcookie.txt $argv[1]) -O - | mplayer -cache 8192 -cookies -cookies-file /tmp/ytcookie.txt -
 end
