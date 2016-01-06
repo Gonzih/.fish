7061461a1f006060a578350ee4adbf335de4960d
Max Gonzih <gonzih@gmail.com>
2016-01-06 16:08:19 +0100

    Use vlc for youtube

--- functions/youtube-stream.fish
+++ functions/youtube-stream.fish
@@ -1,3 +1,3 @@
 function youtube-stream
-	wget (youtube-dl --format 22/17/18 --get-url --cookies=/tmp/ytcookie.txt $argv[1]) -O - | mplayer -cache 8192 -cookies -cookies-file /tmp/ytcookie.txt -
+	vlc -I dummy (youtube-dl --format 22/17/18 --get-url --cookies=/tmp/ytcookie.txt $argv[1])
 end
