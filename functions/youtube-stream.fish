85c96873d6f3500cd6ff4f67b26c04aa023ee133
Max Gonzih <gonzih@gmail.com>
2016-01-10 12:23:02 +0100

    Use vlc for youtube (easier scrolling)

--- functions/youtube-stream.fish
+++ functions/youtube-stream.fish
@@ -1,3 +1,3 @@
 function youtube-stream
-	cvlc (youtube-dl --format 22/17/18 --get-url --cookies=/tmp/ytcookie.txt $argv[1])
+	vlc --play-and-exit (youtube-dl --format 22/17/18 --get-url --cookies=/tmp/ytcookie.txt $argv[1])
 end
