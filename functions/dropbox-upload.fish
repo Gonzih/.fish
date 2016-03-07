599cc99d169631f6a1e7982566c436eab81e2956
Max Gonzih <gonzih@gmail.com>
2016-03-07 15:49:30 +0100

    Make sure that dir exists and that we cleanup old files from time to time

--- functions/dropbox-upload.fish
+++ functions/dropbox-upload.fish
@@ -2,6 +2,8 @@ function dropbox-upload
 	set -l uuid (uuidgen)
 	set -l extension (echo $argv[1] | sed 's/^.*\.\(.*\)$/\1/')
 	set -l fname "$uuid.$extension"
+	mkdir -p ~/Dropbox/Public/tmp/
 	cp $argv[1] ~/Dropbox/Public/tmp/$fname
 	echo https://dl.dropboxusercontent.com/u/4109351/tmp/$fname | xclip -selection -i
+	find ~/Dropbox/Public/tmp -type f -mtime +30 -delete
 end
