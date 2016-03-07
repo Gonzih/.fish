29d810f92a0e61f30071c0daed2eb79b672e64c6
Max Gonzih <gonzih@gmail.com>
2016-03-07 15:46:00 +0100

    Add new function to upload crap to dropbox

---
+++ functions/dropbox-upload.fish
@@ -0,0 +1,7 @@
+function dropbox-upload
+	set -l uuid (uuidgen)
+	set -l extension (echo $argv[1] | sed 's/^.*\.\(.*\)$/\1/')
+	set -l fname "$uuid.$extension"
+	cp $argv[1] ~/Dropbox/Public/tmp/$fname
+	echo https://dl.dropboxusercontent.com/u/4109351/tmp/$fname | xclip -selection -i
+end
