40e6bc02fd83b8e7605e38bf866f140604b74c88
Max Gonzih <gonzih@gmail.com>
2016-03-15 10:29:19 +0100

    Revert "Also allow to switch layout via rigth CTRL"
    This reverts commit d41dd4a89dda38ad12675c96f34f5b19211c7a4e.

--- functions/myxkbmap.fish
+++ functions/myxkbmap.fish
@@ -1,3 +1,3 @@
 function myxkbmap
-	setxkbmap -option -option ctrl:nocaps -option grp:lctrl_toggle -option grp:rctrl_toggle 'us(dvp),ru'
+	setxkbmap -option -option ctrl:nocaps -option grp:lctrl_toggle 'us(dvp),ru'
 end
