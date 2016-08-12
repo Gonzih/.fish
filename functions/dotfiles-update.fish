59d956cda27e5be4b2b87a65e00aa3264867919f
Max Gonzih <gonzih@gmail.com>
2016-08-12 21:19:26 +0200

    Update dotfiles better!

--- functions/dotfiles-update.fish
+++ functions/dotfiles-update.fish
@@ -1,7 +1,16 @@
 function dotfiles-update
-	for dir in ~ ~/.vim ~/.config/mc ~/.config/fish ~/.xmonad ~/.mutt
-		echo "Updating $dir"
-		cd $dir; and git pull
+	set -l dirs ~ ~/.vim ~/.config/mc ~/.config/fish ~/.xmonad ~/.mutt
+
+	for dir in $dirs
+		echo "Stashing $dir"
+		cd $dir; and git stash
+	end
+
+	cd ~; and sudo ansible --ask-sudo-pass provision.yml
+
+	for dir in $dirs
+		echo "Stashing $dir"
+		cd $dir; and git stash pop
 	end
 
 	rbenv-update
