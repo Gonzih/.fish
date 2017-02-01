d282b00978a7d58856082aa1856186e8e311ee94
Max Gonzih <gonzih@gmail.com>
2017-02-01 20:28:16 +0100

    Better font resizing

--- functions/fsize.fish
+++ functions/fsize.fish
@@ -1,3 +1,6 @@
 function fsize
-	printf '\33]50;%s%d\007' "xft:Iosevka Term:style=medium:size=" $argv[1]
+	printf '\e]710;%s%d\007' "xft:Iosevka Term:style=medium:size=" $argv[1]
+	printf '\e]711;%s%d\007' "xft:Iosevka Term:style=bold:size="   $argv[1]
+	printf '\e]712;%s%d\007' "xft:Iosevka Term:style=italic:size=" $argv[1]
+	printf '\e]713;%s%d\007' "xft:Iosevka Term:style=medium:size=" $argv[1]
 end
