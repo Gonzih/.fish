6c3217a1751875d0ddec93c772326781412975fe
Max Gonzih <gonzih@gmail.com>
2017-02-01 20:33:07 +0100

    Super nice font size adjustments

--- config.fish
+++ config.fish
@@ -3,12 +3,17 @@ if test $TERM != "screen-256color"
 end
 
 if test (which rbenv 2>/dev/null)
-  status --is-interactive; and . (rbenv init -|psub)
+	status --is-interactive; and . (rbenv init -|psub)
 end
 
 set -g fish_key_bindings fish_user_key_bindings
 set -g fish_bind_mode insert
 
 set -gx GOPATH /home/gnzh/go-projects
+
+set font_size_file ~/.urxvt-font-size
+if test -f $urxvt_font_size_file
+	fsize (cat $urxvt_font_size_file)
+end
 
 aliases
--- functions/fsize.fish
+++ functions/fsize.fish
@@ -1,4 +1,5 @@
 function fsize
+	echo Use $urxvt_font_size_file to permanently adjust the size
 	printf '\e]710;%s%d\007' "xft:Iosevka Term:style=medium:size=" $argv[1]
 	printf '\e]711;%s%d\007' "xft:Iosevka Term:style=bold:size="   $argv[1]
 	printf '\e]712;%s%d\007' "xft:Iosevka Term:style=italic:size=" $argv[1]
