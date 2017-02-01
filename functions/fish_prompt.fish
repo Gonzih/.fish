a1304f2fc8b560c1f5f3a41253c030fbaddc66c2
Max Gonzih <gonzih@gmail.com>
2017-02-01 20:46:27 +0100

    Make different background symbol

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -134,7 +134,7 @@ function prompt_status -d "the symbols for a non zero exit status, root and back
 
     # Jobs display
     if [ (jobs -l | wc -l) -gt 0 ]
-      prompt_segment black cyan "⚙"
+      prompt_segment black cyan "⊛"
     end
 end
 
