1dd8b59788bbce091724b2e34189f587bf45dba0
Max Gonzih <gonzih@gmail.com>
2017-02-23 18:28:18 +0100

    Cleanup

--- functions/dotfiles-update.fish
+++ functions/dotfiles-update.fish
@@ -1,17 +1,5 @@
 function dotfiles-update
-	set -l dirs ~ ~/.vim ~/.config/mc ~/.config/fish ~/.xmonad ~/.mutt
-
-	for dir in $dirs
-		echo "Stashing $dir"
-		cd $dir; and git stash
-	end
-
 	cd ~; and git pull; and ansible-playbook provision.yml
-
-	for dir in $dirs
-		echo "Stashing $dir"
-		cd $dir; and git stash pop
-	end
 
 	rbenv-update
 	dein-update
