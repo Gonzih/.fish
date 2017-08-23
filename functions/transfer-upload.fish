430495d766aa4b19e2e3a3e26d1ec03836c5c871
Max Gonzih <gonzih@gmail.com>
2017-08-23 15:41:35 +0200

    Reduce screenshots lifetime to 3 days

--- functions/transfer-upload.fish
+++ functions/transfer-upload.fish
@@ -1,7 +1,8 @@
+# Defined in /tmp/fish.x554WN/transfer-upload.fish @ line 2
 function transfer-upload
 	set -l uuid (uuidgen)
 	set -l extension (echo $argv[1] | sed 's/^.*\.\(.*\)$/\1/')
 	set -l fname "$uuid.$extension"
 
-	curl --silent --fail -H 'Max-Downloads: 1000'  -H 'Max-Days: 7' --upload-file $argv[1] https://transfer.sh/$fname | xclip -selection clipboard -i
+	curl --silent --fail -H 'Max-Downloads: 1000'  -H 'Max-Days: 3' --upload-file $argv[1] https://transfer.sh/$fname | xclip -selection clipboard -i
 end
