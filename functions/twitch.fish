ef29de9d1524a0d4cb94a226ff75ad627d61eff4
Max Gonzih <gonzih@gmail.com>
2018-02-10 21:59:31 -0500

    Take extra arguments

--- functions/twitch.fish
+++ functions/twitch.fish
@@ -1,3 +1,9 @@
 function twitch
-	streamlink "https://twitch.tv/$argv[1]" best
+	if test -n "$argv[2]"
+		set quality $argv[2]
+	else
+		set quality best
+	end
+
+	echo streamlink "https://twitch.tv/$argv[1]" $quality
 end
