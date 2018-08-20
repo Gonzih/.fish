c69d828d448701653272d514ebf486f777fa16df
Max Gonzih <gonzih@gmail.com>
2018-08-19 23:02:56 -0400

    Cleanup

--- functions/dotfiles-update.fish
+++ functions/dotfiles-update.fish
@@ -1,10 +1,3 @@
 function dotfiles-update
-	cd ~; and git pull; and ansible-playbook provision.yml
-
-	rbenv-update
-	dein-update
-	xmonad --recompile; and env DISPLAY:=0 xmonad --restart
-	env DISPLAY:=0 xrdb -merge ~/.Xresources
-
-	cd
+	cd ~; and git pull; and ansible-playbook provision.yml; and p
 end
--- functions/rbenv-update.fish
+++
@@ -1,4 +0,0 @@
-function rbenv-update
-    cd ~/.rbenv/plugins/ruby-build; and git pull
-    cd ~/.rbenv/; and git pull
-end
