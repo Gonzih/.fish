584c9c5aa32289810c71af0ec95441433c07e1e4
Max Gonzih <gonzih@gmail.com>
2018-09-08 08:40:15 -0400

    Cleanup main config

--- functions/start-gpg-agent.fish
+++ functions/start-gpg-agent.fish
@@ -1,5 +1,3 @@
 function start-gpg-agent
-	set -l envfile "$HOME/.gnupg/gpg-agent.env"
-
-	gpg-agent --daemon --enable-ssh-support --write-env-file "$envfile"
+	gpg-agent --daemon --enable-ssh-support
 end
