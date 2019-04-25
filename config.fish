8791afa2eba9a339668184d58873a978537edf87
Max Gonzih <gonzih@gmail.com>
2019-04-24 22:39:33 -0400

    Enable go modules

--- config.fish
+++ config.fish
@@ -23,6 +23,7 @@ start-gpg-agent
 
 myxkbmap
 
+set -Ux GO111MODULE on
 set -Ux fish_user_paths /home/gnzh/bin /run/wrappers/bin /home/gnzh/.nix-profile/bin /nix/var/nix/profiles/default/bin /run/current-system/sw/bin /etc/profiles/per-user/gnzh/bin /home/gnzh/go/bin/
 # /home/gnzh/.vim/bundle/dein.vim/repos/github.com/liquidz/vim-iced/bin
 # /home/gnzh/.yarn/bin
