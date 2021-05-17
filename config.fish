1fe774d85e84ff4c69915a8f9266b6698476afa1
Max Gonzih <gonzih@gmail.com>
2021-05-17 17:36:54 -0400

    Python shython

--- config.fish
+++ config.fish
@@ -25,7 +25,7 @@ myxkbmap
 
 set -Ux GO111MODULE on
 set -Ux NPM_PACKAGES $HOME/.npm-packages
-set -Ux fish_user_paths $HOME/bin /run/wrappers/bin $HOME/.nix-profile/bin /nix/var/nix/profiles/default/bin /run/current-system/sw/bin /etc/profiles/per-user/gnzh/bin $HOME/go/bin/ $HOME/.cargo/bin/ $HOME/.linkerd2/bin $HOME/.npm-packages/bin $HOME/.pub-cache/bin
+set -Ux fish_user_paths $HOME/bin /run/wrappers/bin $HOME/.nix-profile/bin /nix/var/nix/profiles/default/bin /run/current-system/sw/bin /etc/profiles/per-user/gnzh/bin $HOME/go/bin/ $HOME/.cargo/bin/ $HOME/.linkerd2/bin $HOME/.npm-packages/bin $HOME/.pub-cache/bin $HOME/.local/bin
 set -gx EDITOR nvim
 
 set fish_function_path $fish_function_path $HOME/.config/plugin-foreign-env/functions
