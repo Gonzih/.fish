8e4bb278a77c7e26e07217dc2c9db5dd7e48db78
Max Gonzih <gonzih@gmail.com>
2022-01-24 19:50:02 -0800

    Add emacs aliases

--- functions/aliases.fish
+++ functions/aliases.fish
@@ -1,10 +1,12 @@
 function aliases
-	alias p=prevd
-	alias n=nextd
-	alias e="$EDITOR"
-	alias em="emacsclient -t"
-	alias tailf="tail -f"
-	alias k=kubectl
-	alias kx=kubectx
-	alias kns=kubens
+    alias p=prevd
+    alias n=nextd
+    alias e="$EDITOR"
+    alias em="emacsclient -t"
+    alias ec="emacsclient --create-frame"
+    alias emacs-stop-daemon="emacsclient -e '(kill-emacs)'"
+    alias tailf="tail -f"
+    alias k=kubectl
+    alias kx=kubectx
+    alias kns=kubens
 end
